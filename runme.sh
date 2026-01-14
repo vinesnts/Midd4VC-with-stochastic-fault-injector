b503b8c3473601d6fa76c825e36194c53f9fb14f
Vinícius Almeida <contact@viniciusalmeida.dev>
2026-01-14 16:35:35 -0300

    💥 fix: Running on linux now creates separate log per experiment

--- runme.sh
+++ runme.sh
@@ -7,8 +7,8 @@ SECONDS_IN_HOUR=3600
 for i in $(seq 1 $N_EXP); do
     EXP_DIR="$(pwd)/experiments/$(date +%Y%m%d_%H%M%S)"
     echo "Run experiment: $i/$N_EXP - $EXP_DIR"
-    python server/Midd4VCServer.py f $EXP_DIR >> server.log 2>> server.log &
-    python client/vehicles.py f $EXP_DIR >> vehicles.log 2>> vehicles.log &
+    python server/Midd4VCServer.py f $EXP_DIR >> $EXP_DIR/server.log 2>> $EXP_DIR/server.log &
+    python client/vehicles.py f $EXP_DIR >> $EXP_DIR/vehicles.log 2>> $EXP_DIR/vehicles.log &
     sleep $SECONDS_IN_HOUR
     echo "Run experiment: $i/$N_EXP: Finished"
 done
