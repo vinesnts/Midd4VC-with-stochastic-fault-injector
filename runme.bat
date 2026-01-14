eb56b6c7c5857f496f79550868ffc99823e22ae4
Vinícius Almeida <contact@viniciusalmeida.dev>
2026-01-14 13:02:55 -0300

    ✨ feat: Adding runme.bat for running on windows

---
+++ runme.bat
@@ -0,0 +1,43 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+REM =========================
+REM CONFIG
+REM =========================
+cd /d C:\Users/vsa/Documents/Midd4VC-with-stochastic-fault-injector
+
+REM Activate virtual environment
+call .venv\Scripts\activate.bat
+
+REM Run applications client in background
+start /B cmd /c "python client\applications.py >> applications.log 2>>&1"
+
+REM Number of experiments
+set N_EXP=10
+set SECONDS_IN_HOUR=3600
+
+REM =========================
+REM LOOP
+REM =========================
+for /L %%i in (1,1,%N_EXP%) do (
+
+    REM Generate timestamp YYYYMMDD_HHMMSS
+    for /f %%t in ('powershell -NoProfile -Command "Get-Date -Format yyyyMMdd_HHmmss"') do (
+        set TIMESTAMP=%%t
+    )
+
+    set EXP_DIR=%CD%\experiments\!TIMESTAMP!
+
+    echo Run experiment: %%i/%N_EXP% - !EXP_DIR!
+
+    REM Run server and vehicles in background
+    start /B cmd /c "python server\Midd4VCServer.py f !EXP_DIR! >> server.log 2>>&1"
+    start /B cmd /c "python client\vehicles.py f !EXP_DIR! >> vehicles.log 2>>&1"
+
+    REM Sleep 600 seconds
+    timeout /t !SECONDS_IN_HOUR! /nobreak >nul
+
+    echo Run experiment: %%i/%N_EXP%: Finished
+)
+
+endlocal
